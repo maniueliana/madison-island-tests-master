f002c7353da24bacfc0d842c135164accd98b994
elianamaniu <38328916+elianamaniu@users.noreply.github.com>
2019-03-06 20:00:57 +0200

    studied parameterized tests

--- src/test/java/org/fasttrackit/SearchTest.java
+++ src/test/java/org/fasttrackit/SearchTest.java
@@ -3,21 +3,35 @@ package org.fasttrackit;
 import org.fasttrackit.pageobjects.Header;
 import org.fasttrackit.pageobjects.ProductsGrid;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+@RunWith(Parameterized.class)
 public class SearchTest extends TestBase {
+
+    private  String keyword;
+
+    public SearchTest(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Parameterized.Parameters
+    public static List<String> inputData(){
+        return Arrays.asList("vase","camera");
+    }
 
     @Test
     public void searchByOneKeywordTest() {
         Header header =
                 PageFactory.initElements(driver, Header.class);
-
-        String keyword = "vase";
         header.search(keyword);
 
         String searchFieldValue = header.getSearchField().getAttribute("value");
